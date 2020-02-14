38b455cb10c08efff6e8bd8e9482ab409cc00279
Vladislav Rakitski <rakitski457@gmail.com>
2020-02-14 13:44:20 +0300

    connect to Postgres DB

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const express = require("express")
-const config = require("config")
+const express = require('express')
+const config = require('config')
+const {Client} = require('pg')
 
 const app = express()
-const PORT = config.get("port") || 5000
+const PORT = config.get('port') || 5000
+
+const bids = new Client({
+  user: 'postgres',
+  password: 'postgres',
+  host: config.get('dataHost'),
+  port: config.get('dataPort'),
+  database: config.get('dataBase')
+})
 
 async function start() {
-  try {
-    app.use('/', express.static(__dirname + '/dist'))
-    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+  try{
+    await bids.connect()
+    console.log("Connected successfully.")
+    const {rows} = await bids.query("SELECT * FROM bids")
+    console.table(rows)
   } catch (e) {
-    console.log('Server Error', e.message)
-    process.exit(1)
+    console.log(`Something wrong happend ${e}`)
+  } finally {
+    await bids.end()
+    console.log("Client disconnected successfully.")    
   }
 }
 
