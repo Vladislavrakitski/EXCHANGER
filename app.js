85caefa0a09bfe026953486129ed556f624f00b1
Vladislav Rakitski <rakitski457@gmail.com>
2020-02-14 12:49:08 +0300

    delete mongo dep

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const config = require("config");
 
 const app = express();
@@ -7,11 +6,6 @@ const PORT = config.get("port") || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(config.get('mongoUri'), {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    });
     app.use('/', express.static(__dirname + '/dist'));
     app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
   } catch (e) {
