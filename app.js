39c5d1cca41bce2a99ea2028cc66f4c889848323
Vladislav Rakitski <rakitski457@gmail.com>
2020-02-15 00:44:16 +0300

    add methods for connect to DB

--- app.js
+++ app.js
@@ -13,18 +13,37 @@ const bids = new Client({
   database: config.get('dataBase')
 })
 
-async function start() {
+//app.listen(PORT, () => console.log(`Web server has been started on port ${PORT}`))
+
+async function getAllBids() {
   try{
     await bids.connect()
-    console.log("Connected successfully.")
-    const {rows} = await bids.query("SELECT * FROM bids")
+    console.log('Connected successfully.')
+    const {rows} = await bids.query('SELECT * FROM bids')
     console.table(rows)
   } catch (e) {
     console.log(`Something wrong happend ${e}`)
   } finally {
     await bids.end()
-    console.log("Client disconnected successfully.")    
+    console.log('Client disconnected successfully.')    
   }
 }
 
-start()
+async function createBid(bid){
+  try {
+    await bids.connect()
+    await bids.query('INSERT INTO bids VALUES ($1)', [bid])
+  } catch(e) {
+    console.log(`Something wrong happend ${e}`)
+  } finally {
+    await bids.end()
+    date = null;
+    console.log('Client disconnected successfully.')    
+  }
+}
+
+getAllBids()
+
+
+
+
