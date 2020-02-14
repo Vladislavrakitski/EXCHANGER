611a2f815e83fdbb40dfd5daa492c497bb4c28f8
Vladislav Rakitski <rakitski457@gmail.com>
2020-02-14 12:58:54 +0300

    fix some bugs

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const config = require("config");
+const express = require("express")
+const config = require("config")
 
-const app = express();
-const PORT = config.get("port") || 5000;
+const app = express()
+const PORT = config.get("port") || 5000
 
 async function start() {
   try {
-    app.use('/', express.static(__dirname + '/dist'));
-    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+    app.use('/', express.static(__dirname + '/dist'))
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
   } catch (e) {
-    console.log('Server Error', e.message);
-    process.exit(1);
+    console.log('Server Error', e.message)
+    process.exit(1)
   }
-};
+}
 
-start();
+start()
