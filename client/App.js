f368674aea10bd7be4f105c04209ce768b3c51bb
Vladislav Rakitski <rakitski457@gmail.com>
2020-02-17 17:17:25 +0300

    add simple request to server

--- client/App.js
+++ client/App.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import {BrowserRouter as Router} from 'react-router-dom';
-import {useRoutes} from './routes';
 
 function App () {
-  const routes = useRoutes()
 
-  return(
-    <Router>
-      <div>
-        {routes}
-      </div>
-    </Router>
+  getBids()
+
+  async function getBids () {
+    try{
+      const result = await fetch("http://localhost:5000/main", {method:"GET", mode:'no-cors'})
+      const bids = await result.json();
+      console.log(bids[0])
+    } catch (e) {
+      console.log("Error reading the bids.")
+    }
+  }
+
+  return (
+    <div>
+      <h1>hello there!</h1>
+    </div>
   )
 }
 
